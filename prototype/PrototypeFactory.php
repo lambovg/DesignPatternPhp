04fb19e611ec3f473e83c27c6bd6e1cb34c6d2b2
georgi lambov <gogo_luxecs@yahoo.com>
2009-12-04 13:53:08 +0200

    Prototype pattern

---
+++ prototype/PrototypeFactory.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * PrototypeFactory
+ */
+abstract class PrototypeFactory 
+{
+	/**
+	 * 
+	 * @access public
+	 * @return void
+	 */
+	public function __clone()
+	{
+	  $this = clone $this;
+
+	  return $this;
+	}
+	
+	/**
+	 * @access public
+	 * @return void
+	 */
+	abstract public function __construct(int $x);
+	
+	/**
+	 * @access public
+	 * @return void
+	 */
+	public function printValue();
+}
+?>
---
+++ prototype/PrototypeImplementation.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * PrototypeImplementation 
+ */
+class PrototypeImplementation 
+extends PrototypeFactory
+{
+  private
+    $x = 0;
+  
+	/**
+	 * 
+	 * @access public
+	 * @return void
+	 */
+	public function __construct()
+	{
+		
+	}
+}
+?>
